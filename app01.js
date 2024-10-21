7b42a875c6c42a67ccea062d8ce22515b214c798
NataliaZegarra <a20170112@juan23.edu.pe>
2024-10-20 22:30:52 -0500

    Add files via upload

---
+++ app01.js
@@ -0,0 +1,5 @@
+document.write("<h1>Bienvenido a la pagina de Java Script</h1>");
+var nom = prompt("Ingresa tu nombre");
+document.write(
+  "Hola " + nom + " es tu primer contacto con la programación en Java Script"
+);
